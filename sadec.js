2f7d407ca16039c7f92bf75ab7daced3b67d0e7d
khang004 <minhkhang102004@gmail.com>
2025-06-22 14:54:47 +0700

    Add files via upload

---
+++ sadec.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', function () {
+  const content = document.getElementById('content');
+  content.classList.add('show');
+});
+window.addEventListener('load', function () {
+  const content = document.getElementById('flip');
+  content.classList.add('show');
+});
+window.addEventListener('load', function () {
+  const content = document.getElementById('zoom');
+  content.classList.add('show');
+});
